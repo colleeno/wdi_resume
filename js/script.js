bd8e071afb59299a2bd252cf7c00b14386feaef8
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-07-29 13:46:16 -0400

    added resume, animated pipe on homepage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ console.log('linked')
 $('.hamburger').on('click', function () {
   $('.nav-links').toggleClass('show-nav')
 })
+
+$(document).ready(function(){
+    $('.divider').slideDown('slow', 'linear')
+});
